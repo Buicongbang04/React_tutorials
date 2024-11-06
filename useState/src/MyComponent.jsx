4060f269d93efbf5749de4195ce12ec532f8ecfd
Buicongbang04 <buicongbang1010@gmail.com>
2024-11-06 18:52:17 +0700

    use state

---
+++ useState/src/MyComponent.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+
+function MyComponent() {
+    const [name, setName] = useState("Guest");
+    const [age, setAge] = useState(0);
+    const [isEmployed, setIsEmployed] = useState(false);
+
+    const updateName = () => {
+        setName("BangBui");
+
+    };
+
+    const incrementAge = () => {
+        setAge(age + 1);
+    };
+
+    const toggleEmployed = () => {
+        setIsEmployed(!isEmployed);
+    };
+
+    return (
+        <>
+            <div>
+                <p>Name: {name}</p>
+                <button onClick={updateName}>Set Name</button>
+
+                <p>Age: {age}</p>
+                <button onClick={incrementAge}>Set Age</button>
+
+                <p>Employed: {isEmployed ? "Yes" : "No"}</p>
+                <button onClick={toggleEmployed}>Toggle Employment</button>
+            </div>
+        </>
+    );
+}
+
+export default MyComponent;
